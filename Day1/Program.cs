b317717a96cb0e53737b848ea0c7d174f844e4c9
Ankie Bosman <ankie555@hotmail.com>
2019-12-01 21:26:28 +0100

    Add project files.
    (cherry picked from commit 2153d39e1f69046a35c993f372722cb0a9a9341d)

---
+++ Day1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = File.ReadAllLines(@"Input.txt");
+            var modules = input.Select(c => int.Parse(c.ToString())).ToList();
+            var requiredFuel = modules.Sum(module =>
+            {
+                var fuel = module / 3 - 2;
+                Console.WriteLine($"Mass of {module}, required fuel {fuel}");
+                return fuel;
+
+            });
+            Console.WriteLine($"Total required fuel: {requiredFuel}");
+        }
+    }
+}
