b9d0b83e1f2bd83e1af9f0e8be76a9b93298e43f
Ankie Bosman <ankie555@hotmail.com>
2019-12-01 22:19:02 +0100

    Part Two
    (cherry picked from commit dce378a5471ce1d570c7646663dfc645a678c576)

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -13,9 +13,19 @@ namespace Day1
             var requiredFuel = modules.Sum(module =>
             {
                 var fuel = module / 3 - 2;
-                Console.WriteLine($"Mass of {module}, required fuel {fuel}");
+                int extraFuel;
+                var inputMass = fuel;
+                do
+                {
+                    extraFuel = inputMass / 3 - 2;
+                    if (extraFuel < 0)
+                    {
+                        extraFuel = 0;
+                    }
+                    fuel += extraFuel;
+                    inputMass = extraFuel;
+                } while (extraFuel>0);
                 return fuel;
-
             });
             Console.WriteLine($"Total required fuel: {requiredFuel}");
         }
